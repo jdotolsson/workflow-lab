80330df65815ddf89556b15604b73b1ffd3860ff
Johan Olsson <johan.olsson@cab.se>
2022-09-08 21:06:59 +0200

    test

--- exe/pwshCommand.ps1
+++ exe/pwshCommand.ps1
@@ -9,4 +9,7 @@ Set-Location $WorkingDirectory
 Write-Host "Yo"
 $schemaFile = Get-ChildItem "./schema.json" | Select-Object -Expand FullName
 $configFiles = (Get-ChildItem -Filter config.json -Recurse | Select-Object -Expand FullName) -join ","
-. "./Automotive.JsonSchemaValidator.exe" $configFiles $schemaFile
+
+$argss = """$configFiles"" ""$schemaFile""";
+$output = cmd /c Automotive.JsonSchemaValidator.exe $configFiles $schemaFile 2`>`&1
+Write-Host $output
