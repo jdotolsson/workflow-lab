73f5546f2d4af85ce716a80458e4ef5884d6736d
Johan Olsson <johan.olsson@cab.se>
2022-09-08 21:11:57 +0200

    fdas

--- exe/pwshCommand.ps1
+++ exe/pwshCommand.ps1
@@ -10,6 +10,5 @@ Write-Host "Yo"
 $schemaFile = Get-ChildItem "./schema.json" | Select-Object -Expand FullName
 $configFiles = (Get-ChildItem -Filter config.json -Recurse | Select-Object -Expand FullName) -join ","
 
-$argss = """$configFiles"" ""$schemaFile""";
 $output = . "./Automotive.JsonSchemaValidator.exe" $configFiles $schemaFile 2`>`&1
 Write-Host $output
