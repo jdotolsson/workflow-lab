d872409294463ec7da9ed3520e1fe0b88115a5e1
Johan Olsson <johan.olsson@cab.se>
2022-09-08 21:00:41 +0200

    grds

--- exe/pwshCommand.ps1
+++ exe/pwshCommand.ps1
@@ -6,7 +6,7 @@ param (
 )
 
 Set-Location $WorkingDirectory
-
+Write-Host "Yo"
 $schemaFile = Get-ChildItem "./schema.json" | Select-Object -Expand FullName
 $configFiles = (Get-ChildItem -Filter config.json -Recurse | Select-Object -Expand FullName) -join ","
-& "./Automotive.JsonSchemaValidator.exe" $configFiles $schemaFile
+. "./Automotive.JsonSchemaValidator.exe" $configFiles $schemaFile
