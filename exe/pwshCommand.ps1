a105ca4a04b1f0ca5dccf44c864fb86ab8303db8
Johan Olsson <johan.olsson@cab.se>
2022-09-08 21:08:32 +0200

    cds

--- exe/pwshCommand.ps1
+++ exe/pwshCommand.ps1
@@ -11,5 +11,5 @@ $schemaFile = Get-ChildItem "./schema.json" | Select-Object -Expand FullName
 $configFiles = (Get-ChildItem -Filter config.json -Recurse | Select-Object -Expand FullName) -join ","
 
 $argss = """$configFiles"" ""$schemaFile""";
-$output = cmd /c Automotive.JsonSchemaValidator.exe $configFiles $schemaFile 2`>`&1
+$output = & "./Automotive.JsonSchemaValidator.exe" $configFiles $schemaFile 2`>`&1
 Write-Host $output
